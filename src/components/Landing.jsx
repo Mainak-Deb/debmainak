07ba94073bdb0cfa982d5e612e11eba3d2a0f6bd
mainak <debmainak37@gmail.com>
2022-07-29 09:46:05 +0530

    first commit

---
+++ src/components/Landing.jsx
@@ -0,0 +1,57 @@
+import React, { useEffect, useRef,useState } from 'react'
+import './styles/Landing.css'
+import { Redirect } from 'react-router';
+import { Link } from 'react-router-dom';
+
+
+const Landing = () => {
+  const [text, settext] = useState("Hello World")
+  const [text2, settext2] = useState(null)
+
+  const finaltext="<Mainak Deb/>"
+  const characters =
+  "!#$%&'()*+,-./:;<=>?@[]^_`{|}~ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+
+  let iterations = finaltext.length+20;
+  function randomstr() {
+    let n = Math.random();
+    n = n * characters.length;
+    n = Math.floor(n);
+    let out = characters[n];
+    return out;
+  }
+
+  const elemout=useRef(null);
+  
+  useEffect(() => {
+    let count = 0;
+    if(text!==finaltext){
+      setInterval(()=>{
+        let str = "";
+        for (let i = parseInt(count); i < finaltext.length; i++) {
+          str += randomstr();
+        }
+        //console.log();
+        settext(finaltext.slice(0,parseInt(count))+str)
+        if(count>=finaltext.length-4){
+          settext2("Click to continue")
+        }
+        count=count+0.1;
+      }, 50);
+    }
+
+  }, []);
+  
+  return (
+    <>
+      <div id='displayout'>
+      <div id='output' >{text}</div>
+      <Link to={"/home"}  id='output2'>{text2}</Link>
+      </div>
+    </>
+
+  )
+}
+
+export default Landing
